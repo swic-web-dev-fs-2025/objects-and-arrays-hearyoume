fe0060a6a3d7723d2019442f57e8c1f94165cfb5
Amanda Ellis <amanda.ellis002@student.swic.edu>
2025-09-29 15:31:32 -0500

    Refactor functions to only take in one course

--- src/utils.js
+++ src/utils.js
@@ -1,57 +1,37 @@
-const getStudentPercentage = ({ courses, courseId, studentId }) => {
-  const course = courses.find(({ id }) => id === courseId);
-  if (!course) return 0;
-
-  const student = course.students.find(({ id }) => id === studentId);
-  if (!student) return 0;
-
-  const { totalPoints, totalMaxPoints } = student.assignments.reduce(
-    (acc, { points = 0, maxPoints }) => ({
-      totalPoints: acc.totalPoints + points,
-      totalMaxPoints: acc.totalMaxPoints + maxPoints,
-    }),
-    { totalPoints: 0, totalMaxPoints: 0 }
-  );
-
-  return Math.round((totalPoints / totalMaxPoints) * 100);
-};
-
-const getClassAverage = (courses, courseId) => {
-  const foundCourse = courses.find(({ id }) => id === courseId);
-  if (!foundCourse) return 0;
-
-  const totalStudents = foundCourse.students.length;
-  if (totalStudents === 0) return 0;
-
-  return Math.round(
-    foundCourse.students
-      .map(({ id }) =>
-        getStudentPercentage({ courses, courseId, studentId: id })
-      )
-      .reduce((sum, percentage) => sum + percentage, 0) / totalStudents
-  );
-};
-
-const addAssignmentToCourse = ({
-  courses,
-  courseId,
-  assignmentName,
-  maxPoints,
-}) => {
-  const clonedCOURSES = structuredClone(courses);
-
-  const foundCourse = clonedCOURSES.find(({ id }) => id === courseId);
-  if (!foundCourse) return clonedCOURSES;
+const addAssignmentToCourse = ({ course, assignmentName, maxPoints }) => {
+  const clonedCOURSE = structuredClone(course);
 
   const newAssignment = { name: assignmentName, points: null, maxPoints };
-  if (!newAssignment.name || !newAssignment.maxPoints) return clonedCOURSES;
+  if (!newAssignment.name || !newAssignment.maxPoints) return clonedCOURSE;
 
-  foundCourse.students = foundCourse.students.map((student) => ({
+  clonedCOURSE.students = clonedCOURSE.students.map((student) => ({
     ...student,
     assignments: [...student.assignments, newAssignment],
   }));
 
-  return clonedCOURSES;
+  return clonedCOURSE;
+};
+
+const getClassAverage = (course) => {
+  return Math.round(
+    course.students
+      .map(({ id }) => getStudentPercentage(course, id))
+      .reduce((sum, percentage) => sum + percentage, 0) / course.students.length
+  );
+};
+
+const getStudentPercentage = (course, studentId) => {
+  const { totalPoints, totalMaxPoints } = course.students
+    .find(({ id }) => id === studentId)
+    .assignments.reduce(
+      (acc, { points = 0, maxPoints }) => ({
+        totalPoints: acc.totalPoints + points,
+        totalMaxPoints: acc.totalMaxPoints + maxPoints,
+      }),
+      { totalPoints: 0, totalMaxPoints: 0 }
+    );
+
+  return Math.round((totalPoints / totalMaxPoints) * 100);
 };
 
 export { getStudentPercentage, getClassAverage, addAssignmentToCourse };
